c7e920eed40ac5f2eab1581ec0c7239be75119de
iAmWaris97 <haleem.waris@gmail.com>
2022-11-25 02:48:34 +0430

    write methods for the tests

--- solver.rb
+++ solver.rb
@@ -1,1 +1,34 @@
-# to write methods here
+class Solver
+  def factorial(number)
+    if number.zero?
+      1
+    elsif number.negative?
+      'Negative nums are invalid'
+    else
+      number * factorial(number - 1)
+    end
+  end
+
+  def reverse(str)
+    str.reverse
+  end
+
+  def fizzbuzz(number)
+    if (number % 3).zero? && (number % 5).zero?
+      'fizzbuzz'
+    elsif (number % 3).zero?
+      'fizz'
+    elsif (number % 5).zero?
+      'buzz'
+    else
+      number
+    end
+  end
+end
+
+solver = Solver.new
+puts solver.factorial(6)
+puts solver.reverse('Waris')
+puts solver.fizzbuzz(9)
+puts solver.fizzbuzz(25)
+puts solver.fizzbuzz(30)
